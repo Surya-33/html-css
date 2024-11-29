cffcb2ede3a651fec2387f2db48a899f3d4fb461
Surya-33 <suryakrishnafeb132004@gmail.com>
2024-11-29 15:26:57 +0530

    Add files via upload

---
+++ leap.py
@@ -0,0 +1,6 @@
+year1=int(input("enter the starting year"))
+year2=int(input("enter the ending year"))
+for year in range(year1,year2+1):
+    if((year%100==0)or(year%4==0))and((year%100!=0)or(year%400==0)):
+        print(year)
+    
